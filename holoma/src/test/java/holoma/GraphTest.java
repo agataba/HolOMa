0efa3b6eedbbfbe96dd21a5013ec4c80cae2fa44
agataba <agata.barcik@gmail.com>
2015-11-28 10:58:10 +0100

    Update Junit information

---
+++ holoma/src/test/java/holoma/GraphTest.java
@@ -0,0 +1,28 @@
+package holoma;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.apache.flink.graph.Edge;
+import org.apache.flink.graph.Graph;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class GraphTest {
+
+	@Test
+	/*public void test() {
+		fail("Not yet implemented");
+	}
+	*/
+
+	
+	public void graphTestRunning (){
+		GraphCreationPoint graphCreation = new GraphCreationPoint();
+		Graph<String, String, Integer> graph = graphCreation.createGraphFromFile("/src/main/resources/example_edges.csv", "/src/main/resources/example_vertices.csv");
+		Assert.assertTrue(!((List<Edge<String, Integer>>) graph).isEmpty());
+	}
+
+}
