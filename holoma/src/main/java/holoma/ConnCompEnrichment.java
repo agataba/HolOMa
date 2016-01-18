09baf7258f176eef0e1dc51a0ffac222330a79a2
max <maexchen@quantentunnel.de>
2016-01-18 12:22:24 +0100

    structure of 'ConnCompEnrichment' designed

--- holoma/src/main/java/holoma/ConnCompEnrichment.java
+++ holoma/src/main/java/holoma/ConnCompEnrichment.java
@@ -20,6 +20,7 @@ public class ConnCompEnrichment {
 	/** Mapping from edge type to weight. */
 	private final Map<Integer, Float> MAP_WEIGHT;
 	
+	
 	/**
 	 * Constructor.
 	 * @param depth Depth of the enrichment of connected components.
@@ -33,6 +34,7 @@ public class ConnCompEnrichment {
 		this.MAP_WEIGHT=mapWeight;
 	}
 	
+	
 	/**
 	 * Starts the enrichment of the connected component and
 	 * returns the result as a graph.
@@ -42,7 +44,34 @@ public class ConnCompEnrichment {
 	public Graph<String, String, Float> getEnrichedConnComp (Set<String> connComp) {
 		Graph<String, String, Float> enrConnComp = null;
 		
+		// calculate the subgraph, i.e. the connected component plus some structure
+		Graph<String, String, Integer> subgraph = extractSubgraph(connComp);
+		
+		// map the values of the edges from type to weight
+		enrConnComp = mapEdgeValues (subgraph);
+		
 		return enrConnComp;
+	}
+	
+	
+	/**
+	 * Extracts a subgraph from the given graph. The subgraph contains the connected
+	 * component plus structure according the depth <code>DEPTH</code>.
+	 * @param connComp A connected component within <code>GRAPH</code>.
+	 * @return The subgraph around the connected component.
+	 */
+	private Graph<String, String, Integer> extractSubgraph (Set<String> connComp) {
+		return null;
+	}
+	
+	
+	/**
+	 * Maps the edge values from edge type to weight according to <code>MAP_WEIGHT</code>.
+	 * @param g The graph for which the mapping is executed.
+	 * @return A new graph with mapped edges.
+	 */
+	private Graph<String, String, Float> mapEdgeValues (Graph<String, String, Integer> g) {
+		return null;
 	}
 	
 
