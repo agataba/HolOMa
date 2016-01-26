9b15f2713e83a322125aab5fb1712c701bfbdca4
macksimiljan <macksimiljan@users.noreply.github.com>
2016-01-26 19:18:50 +0100

    [init] test class for PageRankEvaluation

---
+++ holoma/src/test/java/holoma/PageRankEvaluationTest.java
@@ -0,0 +1,19 @@
+package holoma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PageRankEvaluationTest {
+
+	@Test
+	public void testSetEvalData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBestFriends() {
+		fail("Not yet implemented");
+	}
+
+}
