6fb102616f32c2931ba8ed6ef815a129826ea436
max <maexchen@quantentunnel.de>
2016-01-26 16:49:59 +0100

    [init] evaluation class for page rank results

---
+++ holoma/src/main/java/holoma/ppr/PageRankEvaluation.java
@@ -0,0 +1,11 @@
+package holoma.ppr;
+
+/**
+ * Evaluates the results of applying the personlized pagerank algorithm
+ * to the enriched connected components.
+ * @author max
+ *
+ */
+public class PageRankEvaluation {
+
+}
