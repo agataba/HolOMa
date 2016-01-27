a63d1ecd6b129ddd2a43e02032ced5b2ffd9edf5
macksimiljan <macksimiljan@users.noreply.github.com>
2016-01-27 19:47:13 +0100

    [updated] best friend method contains now more than only one best friend

--- holoma/src/test/java/holoma/PageRankEvaluationTest.java
+++ holoma/src/test/java/holoma/PageRankEvaluationTest.java
@@ -4,8 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -91,16 +93,24 @@ public class PageRankEvaluationTest {
 
 	@Test
 	public void testGetBestFriends() {
-		Map<String, Tuple2<String,Float>> expectedResult = new HashMap<String, Tuple2<String,Float>>();
+		Map<String, Set<Tuple2<String,Float>>> expectedResult = new HashMap<String, Set<Tuple2<String,Float>>>();
+		Set<Tuple2<String,Float>> bestsOfX = new HashSet<Tuple2<String,Float>>();
 		
 		Tuple2<String,Float> bestFriend = new Tuple2<String, Float>("1",1f);
-		expectedResult.put("1", bestFriend);
+		bestsOfX.add(bestFriend);
+		expectedResult.put("1", bestsOfX);
 		
+		bestsOfX = new HashSet<Tuple2<String,Float>>();
 		bestFriend = new Tuple2<String, Float>("11",0.8f);
-		expectedResult.put("3", bestFriend);
+		bestsOfX.add(bestFriend);
+		expectedResult.put("3", bestsOfX);
 		
+		bestsOfX = new HashSet<Tuple2<String,Float>>();
 		bestFriend = new Tuple2<String, Float>("13",0.8f);
-		expectedResult.put("8", bestFriend);		
+		bestsOfX.add(bestFriend);
+		bestFriend = new Tuple2<String, Float>("11",0.8f);
+		bestsOfX.add(bestFriend);
+		expectedResult.put("8", bestsOfX);		
 		
 		assertEquals(expectedResult, prEval.getBestFriends());
 	}
